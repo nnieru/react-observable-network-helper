24c3e796f6387d9b9349e57db289f6a6070ba9bd
Nathaniel Andrian <nathaniel.andrian@binus.edu>
2024-09-19 16:55:29 +0700

    remove credential type

--- lib/src/InsightDecorator.ts
+++ lib/src/InsightDecorator.ts
@@ -1,15 +1,5 @@
-type InsightCredential = {
-  apiKey: string;
-  projectId: string;
-  version: string;
-  environment: string;
-  user: string;
-  host: string;
-  port: number;
-};
-
 interface IInsightDecorator {
   send(event: string, message: string, data: any): void;
 }
 
-export { InsightCredential, IInsightDecorator };
+export { IInsightDecorator };
